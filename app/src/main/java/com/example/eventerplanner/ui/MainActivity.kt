3af85f650ddee5398cd18816f6e18d62f7869535
Max <m.kotlyakov.02@mail.ru>
2023-04-20 07:18:34 +0300

    second initial commit

--- app/src/main/java/com/example/eventerplanner/ui/MainActivity.kt
+++ app/src/main/java/com/example/eventerplanner/ui/MainActivity.kt
@@ -3,11 +3,14 @@ package com.example.eventerplanner.ui
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.eventerplanner.presentation.theme.EventerPlannerTheme
@@ -17,8 +20,19 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             EventerPlannerTheme {
-
+                Greeting()
             }
         }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Hello, Android")
     }
 }
